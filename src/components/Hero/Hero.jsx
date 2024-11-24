ff9eccbabb4e27295b71d7f780eccf876a9017fa
AndrewChacon <andrew.b.chacon@gmail.com>
2024-11-24 16:41:39 -0500

    glitch effect on hero title

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,16 @@ import './Hero.css';
 const Hero = () => {
 	return (
 		<div className='hero'>
-			<p className='hero-title glitch'>
+			<p data-text='ANDREW' className='glitch hero-title'>
 				ANDREW
-				<br />
+			</p>
+			<p data-text='CHACON_' className='glitch hero-title bottom'>
 				CHACON_
 			</p>
-			<p className='hero-desc'>
+			<h1 className='hero-desc'>
 				Im just a chill guy that low key doesnt give a fuck and i make
 				websites on the on my free time
-			</p>
+			</h1>
 
 			<button className='hero-button'>Github_</button>
 		</div>
